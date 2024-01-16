b3bc1f6b1360d561f7b36fd09032143d447bb0c7
Raluca Enea <ralucaenea@Ralucas-MacBook-Pro.local>
2024-01-17 00:28:25 +0200

    cart page doua butoane

--- src/cart/CartPage.js
+++ src/cart/CartPage.js
@@ -2,12 +2,18 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import "./CartPage.css"
+import OrderConfirmationModal from './OrderConfirmationModal';
+import CreateOrder from './CreateOrder';
+
 
 const CartPage = ({ isLoggedIn, setCurrentPage}) => {
   const [cartProducts, setCartProducts] = useState([]);
   const [idUser, setIdUser] = useState('');
   const [idOrder, setIdOrder] = useState('')
   const [total, setTotal] = useState(0)
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [showCreateOrder, setShowCreateOrder] = useState(false);
+  const [orderPlaced, setOrderPlaced] = useState(false);
   const token = localStorage.getItem('token')
   
   useEffect(() => {
@@ -62,6 +68,39 @@ const CartPage = ({ isLoggedIn, setCurrentPage}) => {
     fetchUserId()
   }, []); 
   
+  const handlePlaceOrder = async (idOrd) => {
+    try {
+      let userConfirmed;
+  
+      userConfirmed = window.confirm(`Are you sure you want to place the order?`);
+      if (userConfirmed) {        
+  
+        setShowCreateOrder(true);
+  
+        setOrderPlaced(true);
+        setCartProducts([]);
+      } else {
+        console.log('Order placement canceled by the user.');
+      }
+    } catch (error) {
+      console.error('Error placing order:', error);
+    }
+  };
+
+  const handleRemoveProduct = async (productId) => {
+    try {
+      // Implementează logica pentru ștergerea produsului din coș
+      const response = await axios.delete(`http://localhost:8080/orderitems/delete/${productId}`);
+      console.log(response.data);
+      // Reîncarcă lista de produse după ștergere
+      showCartProducts(idOrder);
+    } catch (error) {
+      console.error('Error removing product:', error);
+    }
+  };
+
+
+
   return (
     <div className='cartPage'>
       {isLoggedIn && (
@@ -71,18 +110,28 @@ const CartPage = ({ isLoggedIn, setCurrentPage}) => {
             {cartProducts.map((product, index) => (
               <li key={index}>
                 <div className='cartItem'>
-                  <img className='cartImg' src={`products/${product.a.photo}`} alt="product"/>
+                <img className='cartImg' src={`products/${product.a.photo}`} alt="product"/>
                   <div>
                     <p>Product: {product.a.name}</p>
                     <p>Price: {`${product.a.price}$`}</p>
                     <p>Quantity:{` ${product.b}`}</p>
+                    <button onClick={() => handleRemoveProduct(product.id)}>Remove</button>
+
                   </div>
                 </div>
               </li>
+              
             ))}
           </ul>
           <h3>{`Total: ${total}$`}</h3>
           {/* <button onClick={clearCart}>Clear Cart</button> */}
+          <button className="placeOrderButton" onClick={handlePlaceOrder}>
+            Place Order
+          </button>
+          {showCreateOrder && (
+            <CreateOrder cartProducts={cartProducts} setShowCreateOrder={setShowCreateOrder} />
+          )}
+          {orderPlaced && <p>Order placed successfully!</p>}
         </>
       )}
       {!isLoggedIn && setCurrentPage('home')}
